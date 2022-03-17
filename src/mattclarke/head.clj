c1355193c2aa174fd6e198069b31f61f7f012d67
mclarke <mclarkestudio@gmail.com>
2022-03-17 18:14:00 -0400

    add global css

--- src/mattclarke/head.clj
+++ src/mattclarke/head.clj
@@ -6,7 +6,7 @@
   (let [author "Matthew Clarke"
         desc "Product lead with a background in art, design and engineering."]
     {:write-dir "public"
-     :css-path "/css/global.css"
+    ;;  :css-path "/css/global.css"
      :site-url "https://matthewclarke.dev"
      :author author
      :title author
@@ -36,13 +36,22 @@
   [:meta {:charset "utf-8"}]
   [:meta {:name "viewport", :content "width=device-width, initial-scale=1"}])
 
+(defn global-css []
+  [:link {:type "text/css", :href "/css/global.css", :rel "stylesheet"}])
+
+(defn global-head
+  "Global head for every page"
+  []
+  (global-fonts)
+  (global-technical)
+  (global-icon-links)
+  (global-css))
+
 (defn make-index-head
   "Make index head from gc, global content"
   []
   (html [:head
-              (global-fonts)
-              (global-technical)
-              (global-icon-links)
+              (global-head)
               [:title (gc :title)]
               [:meta {:name "description", :content (gc :description)}]
               [:meta {:name "author", :content (gc :author)}]
@@ -54,14 +63,10 @@
   "Make html head from markdown page data."
   [metadata]
   (html [:head
-              (global-fonts)
-              (global-technical)
-              (global-icon-links)
+              (global-head)
               [:title (first (metadata :title))]
             ;;   [:meta {:name "description", :content (gc :description)}]
             ;;   [:meta {:name "author", :content (gc :author)}]
             ;;   (open-graph)
             ;;   [:link {:type "text/css", :href (gc :css-path), :rel "stylesheet"}]
               ]))
-
-;; (make-global-head)
